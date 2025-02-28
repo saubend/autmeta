3ee33817027319f53a37d0e46ab97e37a7e5d704
saubend <saubend@gmail.com>
2025-02-28 21:29:01 +0200

    submit

---
+++ Admin/login.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Prisijungimas</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        <div class="login">
+            <h1 class="text-center">Prisijungti</h1>
+            
+        </div>
+    </body>
+</html>
